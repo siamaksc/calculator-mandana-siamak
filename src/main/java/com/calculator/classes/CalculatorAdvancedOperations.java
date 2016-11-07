2bdffb11fb0237b85c57522c9bbdea7c0a329d2d
siamaksc <siamaksc@kth.se>
2016-11-07 11:56:28 +0100

    Button Square Root was added

--- src/main/java/com/calculator/classes/CalculatorAdvancedOperations.java
+++ src/main/java/com/calculator/classes/CalculatorAdvancedOperations.java
@@ -7,19 +7,34 @@ import com.calculator.interfaces.CalculatorAdvancedOperationsInterface;
 public class CalculatorAdvancedOperations implements CalculatorAdvancedOperationsInterface {
 
 	public double procent(double firstNumber, double secondNumber) {
-	
 		double result = firstNumber*(secondNumber/100);
 		return result;
 	}
 
 	public double power(double firstNumber, double secondNumber) {
 		double result= Math.pow(firstNumber, secondNumber);
-		if (Double.isInfinite(result)){
-			JOptionPane.showMessageDialog(null, "Invalid input ", "Error message", JOptionPane.ERROR_MESSAGE);			
+		if (Double.MAX_VALUE == result){
+			JOptionPane.showMessageDialog(null, "MAX VALUE ", "Error message", JOptionPane.ERROR_MESSAGE);			
 			return -1111.1111;
-		
-	}
+		}
 		return result;
-}
+	}
+	
+	public double squareRoot (double firstNumber){
+		if (firstNumber < 0) {
+			JOptionPane.showMessageDialog(null, "Invalid Input ", "Error message", JOptionPane.ERROR_MESSAGE);
+			return -1111.1111;
+		}
+		double result = Math.sqrt(firstNumber);
+		return result;
+	}
+	
+	/*public double fibonacci(double number) {
+			if (number<=1){
+				return number;
+			}else{
+				return fibonacci(number-1)+fibonacci(number-2);
+			}
+	}*/
 	
 }
